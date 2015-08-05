152ac2f45ea27424ab87c07e24c348dac024b82b
YangHui <yanghui@tigerjoys.com>
2015-08-05 10:39:17 +0800

    添加最大化按钮

--- js/helper.js
+++ js/helper.js
@@ -5,10 +5,10 @@
 
     setInterval(function(){
       if(iframeExists()){
-        var $main = $(iframeSelector).parents("td.aui_main");
-        $(iframeSelector).width($main.width());
-        $(iframeSelector).height($main.height() - 10);
-        addButton();
+
+        addFormatButton();
+        addMaxButton();
+
       }
     }, 1000);
 
@@ -17,11 +17,11 @@
       return $iframe.size() > 0;
     }
 
-    function addButton(){
+    function addFormatButton(){
       var $btnCtn = $(".aui_buttons:visible");
       var hasExists = $btnCtn.find(".format_btn").size() > 0;
       if(!hasExists){
-        $btnFormat = $('<button class="aui_state_highlight format_btn" type="button">转换</button>');
+        $btnFormat = $('<button class="aui_state_highlight format_btn" type="button">转码</button>');
 
         $btnCtn.prepend($btnFormat);
 
@@ -33,10 +33,40 @@
     }
 
     function formatList(selector){
-      var $xlist = $iframe.find(selector);
-      var jsonVal = $xlist.val();
+      var $textArea = $iframe.find(selector);
+      var jsonVal = $textArea.val();
       var formatedJson = JSON.stringify(JSON.parse(jsonVal), null, "    ");
-      $xlist.val(formatedJson);
+      $textArea.val(formatedJson);
+    }
+
+    function addMaxButton(){
+      var $content = $(iframeSelector).parents("div.aui_content");
+      $content.width('100%');
+      $content.height('100%');
+      $(iframeSelector).width('100%');
+      $(iframeSelector).height('100%');
+
+      var $titleBar = $(iframeSelector).parents('.aui_dialog').find('.aui_titleBar');
+      if($titleBar.find('.aui_max').size() == 0){
+        $titleBar.find('.aui_close').before('<a class="aui_max aui_close" style="margin-right:20px;" href="javascript:void(0);" style="display: block;">o</a>');
+        $titleBar.find('.aui_max').click(function(){
+          var $win = $(iframeSelector).parents("div.aui_state_focus");
+          $win.css({left: 0, top: 0});
+          $win.width($('body').width());
+          $win.height($('body').height());
+
+          var $main = $(iframeSelector).parents("td.aui_main");
+          $main.width($('body').width());
+          $main.height($('body').height() - 80);
+
+          var $xlist = $iframe.find('#xlist');
+          var $ylist = $iframe.find('#ylist');
+          $xlist.width($('body').width() - 150);
+          $ylist.width($('body').width() - 150);
+          $xlist.height(300);
+          $ylist.height(300);
+        });
+      }
     }
 
   });
